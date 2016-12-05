94cbd713ab85b4792c7d58078c1c3a520361ac5c
RJ Garcia <rj@bighead.net>
2016-12-05 15:06:18 -0800

    Starting 0.2.4-dev
    Signed-off-by: RJ Garcia <rj@bighead.net>

--- src/App.php
+++ src/App.php
@@ -10,7 +10,7 @@ class App implements \ArrayAccess, EventEmitterInterface
 {
     use RouteMatch;
 
-    const VERSION = '0.2.2';
+    const VERSION = '0.2.4-dev';
 
     private $container;
     private $frozen;
